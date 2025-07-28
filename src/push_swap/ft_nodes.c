3c585ecd4443618db4855e5a00cc3d2fd5488302
cuatr0-sh <cuatr0@protonmail.com>
2025-07-28 02:37:52 +0200

    some norm fixes

--- src/push_swap/ft_nodes.c
+++ src/push_swap/ft_nodes.c
@@ -1,4 +1,16 @@
-#include "../includes/push_swap.c"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_nodes.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asoria <asoria@student.42madrid.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/28 02:35:50 by asoria            #+#    #+#             */
+/*   Updated: 2025/07/28 02:37:30 by asoria           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
 
 void	append_node(t_stack *stack, t_node *new)
 {
@@ -20,16 +32,16 @@ void	append_node(t_stack *stack, t_node *new)
 	stack->size++;
 }
 
-t_node  *create_node(int value)
+t_node	*create_node(int value)
 {
-        t_node  *new;
+	t_node	*new;
 
-        new = malloc(sizeof(t_node));
-        if (!new)
-                exit(EXIT_FAILURE);
-        new->value = value;
-        new->index = -1;
-        new->next = NULL;
-        new->prev = NULL;
-        return (new);
+	new = malloc(sizeof(t_node));
+	if (!new)
+		exit(EXIT_FAILURE);
+	new->value = value;
+	new->index = -1;
+	new->next = NULL;
+	new->prev = NULL;
+	return (new);
 }
