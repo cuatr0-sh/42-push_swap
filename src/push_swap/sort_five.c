8a72a4728726f900624e44505fb4a2c7097f0217
cuatr0-sh <cuatr0@icloud.com>
2025-10-22 20:43:09 +0200

    fixed 25 lines limit for sort_five

--- src/push_swap/sort_five.c
+++ src/push_swap/sort_five.c
@@ -6,7 +6,7 @@
 /*   By: asoria <asoria@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 20:32:32 by asoria            #+#    #+#             */
-/*   Updated: 2025/10/22 20:32:33 by asoria           ###   ########.fr       */
+/*   Updated: 2025/10/22 20:41:23 by asoria           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,30 +34,38 @@ static int	find_min_index(t_stack *stack)
 	return (min_i);
 }
 
+static void	push_min_to_b(t_stack **a, t_stack **b, int size)
+{
+	int	min_index;
+	int	i;
+
+	min_index = find_min_index(*a);
+	if (min_index == 0)
+		do_pb(a, b);
+	else if (min_index <= size / 2)
+	{
+		i = min_index;
+		while (i-- > 0)
+			do_ra(a);
+		do_pb(a, b);
+	}
+	else
+	{
+		i = size - min_index;
+		while (i-- > 0)
+			do_rra(a);
+		do_pb(a, b);
+	}
+}
+
 void	sort_five(t_stack **a, t_stack **b)
 {
 	int	size;
-	int	min_index;
 
 	size = ft_get_stack_size(*a);
 	while (size > 3)
 	{
-		min_index = find_min_index(*a);
-		if (min_index == 0)
-			do_pb(a, b);
-		else if (min_index <= size / 2)
-		{
-			while (min_index-- > 0)
-				do_ra(a);
-			do_pb(a, b);
-		}
-		else
-		{
-			min_index = size - min_index;
-			while (min_index-- > 0)
-				do_rra(a);
-			do_pb(a, b);
-		}
+		push_min_to_b(a, b, size);
 		size--;
 	}
 	sort_three(a);
